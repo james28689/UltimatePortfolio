1e69b4c2be42ba6f298f40065dc92a41f42b40fc
James Watling <james.watling@gmail.com>
2020-12-28 16:21:44 +0000

    Changes up to & including episode 3

---
+++ UltimatePortfolio/Views/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by James Watling on 28/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Closed")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
