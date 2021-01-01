b62ca1182f2f47033f9805ceab6f764f5d68bcbf
James Watling <james.watling@gmail.com>
2021-01-01 12:22:05 +0000

    Up to & including episode 10 - some issues with custom Binding onChange method quitting view - fixed with workaround using onDissapear()

---
+++ UltimatePortfolio/Views/ProjectHeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  ProjectHeaderView.swift
+//  UltimatePortfolio
+//
+//  Created by James Watling on 31/12/2020.
+//
+
+import SwiftUI
+
+struct ProjectHeaderView: View {
+    @ObservedObject var project: Project
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(project.projectTitle)
+                
+                ProgressView(value: project.completionAmount)
+                    .accentColor(Color(project.projectColor))
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: EditProjectView(project: project)) {
+                Image(systemName: "square.and.pencil")
+                    .imageScale(.large)
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding(.horizontal, 5)
+        .padding(.bottom, 10)
+    }
+}
+
+struct ProjectHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectHeaderView(project: Project.example)
+    }
+}
