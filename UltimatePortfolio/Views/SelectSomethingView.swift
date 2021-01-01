95156e0272273cb77473d2b3e42f2e34d416f17d
James Watling <james.watling@gmail.com>
2021-01-01 22:35:57 +0000

    Up to & including episode 14

---
+++ UltimatePortfolio/Views/SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  UltimatePortfolio
+//
+//  Created by James Watling on 01/01/2021.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select something from the menu to begin.")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
